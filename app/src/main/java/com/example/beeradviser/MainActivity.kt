cd8096fae7a47d167e719668529a4e18c73a137b
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2026-01-19 12:31:38 +0400

    feat: ru locale added

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val findBeer = findViewById<Button>(R.id.find_beer)
         findBeer.setOnClickListener {
             val beerColor = findViewById<Spinner>(R.id.beer_color)
@@ -21,6 +20,7 @@ class MainActivity : AppCompatActivity() {
             val beers = beerList.reduce { str, item -> str + '\n' + item }
             val brands = findViewById<TextView>(R.id.brands)
             brands.text = beers
+
         }
     }
 
